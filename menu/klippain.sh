2454eb0245b21bccc1012d031fa9e1b795f20047
GAB-3D <gabrielecastellani@hotmail.com>
2024-03-27 01:54:56 +0100

    fix

--- menu/klippain.sh
+++ menu/klippain.sh
@@ -1,2 +1,6 @@
 cd $HOME
-wget -O - https://raw.githubusercontent.com/Frix-x/klippain/main/install.sh | bash
+wget -O - wget -O - https://raw.githubusercontent.com/Frix-x/klippain-shaketune/main/install.sh | bash
+
+#add [include K-ShakeTune/*.cfg] on to of $HOME/printer_data/config/printer.cfg
+echo "[include K-ShakeTune/*.cfg]" >> $HOME/printer_data/config/printer.cfg
+ 
