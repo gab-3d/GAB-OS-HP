76158464dae5f7b21280ac3c3fd6e78d78fd7b75
GAB-3D <gabrielecastellani@hotmail.com>
2024-03-27 02:49:22 +0100

    fix

--- menu/klippain.sh
+++ menu/klippain.sh
@@ -1,6 +1,13 @@
 cd $HOME
 wget -O - wget -O - https://raw.githubusercontent.com/Frix-x/klippain-shaketune/main/install.sh | bash
 
-#add [include K-ShakeTune/*.cfg] on to of $HOME/printer_data/config/printer.cfg
-echo "[include K-ShakeTune/*.cfg]" >> $HOME/printer_data/config/printer.cfg
- 
+#add [include K-ShakeTune/*.cfg] at the beginning of $HOME/printer_data/config/printer.cfg
+
+#check if k-ShakeTune is already included in printer.cfg
+if grep -q "K-ShakeTune" $HOME/printer_data/config/printer.cfg; then
+    echo "K-ShakeTune is already included in printer.cfg"
+    exit 0
+fi
+
+#add [include K-ShakeTune/*.cfg] at the beginning of $HOME/printer_data/config/printer.cfg
+echo "[include K-ShakeTune/*.cfg]" | cat - $HOME/printer_data/config/printer.cfg > temp && mv temp $HOME/printer_data/config/printer.cfg
